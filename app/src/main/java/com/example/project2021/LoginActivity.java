4eb1bf633d6b32cba316ae3bc729697b64914233
yzzzu94 <yzzzu94@naver.com>
2021-01-07 18:26:02 +0900

    Login Layout

--- app/src/main/java/com/example/project2021/LoginActivity.java
+++ app/src/main/java/com/example/project2021/LoginActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         tv = findViewById(R.id.textView_login);
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.button_Login);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
