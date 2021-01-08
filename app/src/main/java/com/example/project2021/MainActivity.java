c4d5c62efd5778638a44cc3af9835f4df2643304
yzzzu94 <yzzzu94@naver.com>
2021-01-08 18:08:31 +0900

    kakao Login

--- app/src/main/java/com/example/project2021/MainActivity.java
+++ app/src/main/java/com/example/project2021/MainActivity.java
@@ -5,9 +5,16 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +26,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavView);
         NavController navController = Navigation.findNavController(this,R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
     }
+
 }
